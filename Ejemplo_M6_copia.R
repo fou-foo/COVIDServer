35c4b28dc1a1c49b294f4a940696df480eaa4b2c
root <root@localhost.localdomain>
2020-04-12 17:26:19 -0500

    se prepara para contonuar con simulacion en el dia 220 con otro script

--- Ejemplo_M6_copia.R
+++ Ejemplo_M6_copia.R
@@ -137,7 +137,7 @@ names(X_ini) <- np  #El orden es super importante
 
 # TIEMPO  ########################################
 
-days <- 160
+days <- 220
 print("days                          **********************")
 print(days)
 tiempos <- seq(0, days ,length = days*2+1)
